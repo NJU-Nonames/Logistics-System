ae152e934f0d51a2432dd9b6f23ba681e70b6d78
abc <947106974@qq.com>
2015-11-26 19:42:34 +0800

    CheckBL实现

--- Logistics_System/Client/src/main/java/businesslogic/chartbl/CheckBLImpl.java
+++ Logistics_System/Client/src/main/java/businesslogic/chartbl/CheckBLImpl.java
@@ -8,6 +8,7 @@ import po.list.HallArrivalListPO;
 import po.list.LoadListPO;
 import po.list.OrderListPO;
 import po.list.TransArrivalListPO;
+import po.list.TransShipmentListPO;
 import po.moneyInfomation.MoneyInListPO;
 import po.moneyInfomation.MoneyOutListPO;
 import po.repertory.GoodsInfoPO;
@@ -24,6 +25,7 @@ import dataservice.moneyInformation.MoneyInListDataService;
 import dataservice.moneyInformation.MoneyOutListDataService;
 import dataservice.repertory.RepertoryInDataService;
 import dataservice.repertory.RepertoryOutDataService;
+import utility.CheckType;
 import utility.DocType;
 import vo.DeliveringListVO;
 import vo.GoodsInfoVO;
@@ -36,6 +38,7 @@ import vo.OrderListVO;
 import vo.RepertoryInVO;
 import vo.RepertoryOutVO;
 import vo.TransArrivalListVO;
+import vo.TransShipmentListVO;
 import businesslogic.rmi.RMIHelper;
 import businesslogicservice.chartblservice.CheckBLService;
 
@@ -124,15 +127,24 @@ public class CheckBLImpl implements CheckBLService {
 		        for(DeliveringListPO po:deliverlistpo){		
 		        	deliverlistvo.add(new DeliveringListVO(po.getId(), po.getDate(), po.getBarCode(), po.getDeliveryMan(), po.getCheckType()));
 		        }
+		        return deliverlistvo;
 			case CENTER_ARRIVAL_NOTE:
 				ArrayList<TransArrivalListPO> transarrivalpo=transarrival.showAll(time, time);
 				ArrayList<TransArrivalListVO> transarrivalvo=new ArrayList<TransArrivalListVO>();
 				for(TransArrivalListPO po:transarrivalpo){
-				ArrayList<GoodsInfoVO> goodvo=null;
+				ArrayList<GoodsInfoVO> goodvo=new ArrayList<GoodsInfoVO>();
 					for(GoodsInfoPO goods:po.getGoodsInfoPOs())
-						goodvo.add(new GoodsInfoVO(goods.getBarcode(), goods., goods.getDeparturePlace()));
-					transarrivalvo.add(new TransArrivalListVO(po.getId(), po.getTransferNumber(), po.getCenterNumber(), po.getDate(), po.getGoodsInfoPOs(), po.getCheckType()));
+						goodvo.add(new GoodsInfoVO(goods.getBarcode(), goods.getState(),goods.getDeparturePlace()));
+					transarrivalvo.add(new TransArrivalListVO(po.getId(), po.getTransferNumber(), po.getCenterNumber(), po.getDate(), goodvo, po.getCheckType()));
 				}
+				return transarrivalvo;
+			case TRANSIT_NOTE:
+				ArrayList<TransShipmentListPO> transshipmentpo=transshipment.showAll(time, time);
+				ArrayList<TransShipmentListVO> transshipmentvo=new ArrayList<TransShipmentListVO>();
+				for(TransShipmentListPO po:transshipmentpo){
+					transshipmentvo.add(new TransShipmentListVO(po.getDate(), po.getTransitDocNumber(), po.getFlightNumber(), po.getDeparturePlace(), po.getDestination(), po.getContainerNumber(), po.getSupercargoMan(), po.getBarcodes(), po.getCheckType()));
+				}
+				return transshipmentvo;		
 			}
 		}catch(RemoteException e){
 			e.printStackTrace();
@@ -142,21 +154,329 @@ public class CheckBLImpl implements CheckBLService {
 
 	public boolean passAllDoc(DocType docType, String time) {
 		// TODO Auto-generated method stub
+		try{
+			switch(docType){
+			case MONEY_IN_NOTE:
+				ArrayList<MoneyInListPO> moneyinpo=moneyin.showAll(time, time);
+				for(MoneyInListPO po:moneyinpo){
+					po.setCheckType(CheckType.PASS);
+					moneyin.update(po);
+				}
+				return true;
+			case MONET_OUT_NOTE:
+				ArrayList<MoneyOutListPO> moneyoutpo=moneyout.showAll(time, time);
+				for(MoneyOutListPO po:moneyoutpo){
+					po.setCheckType(CheckType.PASS);
+					moneyout.update(po);
+				}
+				return true;
+			case IN_STOREAGE_NOTE:
+				ArrayList<RepertoryInPO> repertoryinpo=repertoryin.showAll(time, time);
+				for(RepertoryInPO po:repertoryinpo){
+					po.setCheckType(CheckType.PASS);
+					repertoryin.update(po);
+				}
+				return true;
+			case OUT_STOREAGE_NOTE:
+				ArrayList<RepertoryOutPO> repertoryoutpo=repertoryout.showAll(time, time);
+				for(RepertoryOutPO po:repertoryoutpo){
+					po.setCheckType(CheckType.PASS);
+					repertoryout.update(po);
+				}
+				return true;
+			case SENDING_NOTE:
+				ArrayList<OrderListPO> orderpo=order.showAll(time, time);
+				for(OrderListPO po:orderpo){
+					po.setCheckType(CheckType.PASS);
+					order.update(po);
+				}
+				return true;
+			case LOADING_NOTE:
+				ArrayList<LoadListPO> loadpo=load.showAll(time, time);
+				for(LoadListPO po:loadpo){
+					po.setCheckType(CheckType.PASS);
+					load.update(po);
+				}
+				return true;
+			case BUSINESSHALL_ARRIVAL_NOTE:
+				ArrayList<HallArrivalListPO> hallarrivalpo=hallarrival.showAll(time, time);
+				for(HallArrivalListPO po:hallarrivalpo){
+					po.setCheckType(CheckType.PASS);
+					hallarrival.update(po);
+				}
+				return true;
+			case DELIVERYING_NOTE:
+				ArrayList<DeliveringListPO> deliverlistpo=deliver.showAll(time, time);
+				for(DeliveringListPO po:deliverlistpo){
+					po.setCheckType(CheckType.PASS);
+					deliver.update(po);
+				}
+				return true;
+			case CENTER_ARRIVAL_NOTE:
+				ArrayList<TransArrivalListPO> transarrivalpo=transarrival.showAll(time, time);
+				for(TransArrivalListPO po:transarrivalpo){
+					po.setCheckType(CheckType.PASS);
+					transarrival.update(po);
+				}
+				return true;
+			case TRANSIT_NOTE:
+				ArrayList<TransShipmentListPO> transshipmentpo=transshipment.showAll(time, time);
+				for(TransShipmentListPO po:transshipmentpo){
+					po.setCheckType(CheckType.PASS);
+					transshipment.update(po);
+				}
+				return true;
+			}
+		}catch(RemoteException e){
+			e.printStackTrace();
+		}
 		return false;
 	}		
 
 	public boolean passSelectedDoc(DocType docType, ArrayList<? extends ListVO> listVO) {
 		// TODO Auto-generated method stub
+		try{
+			switch(docType){
+			case MONEY_IN_NOTE:
+				ArrayList<MoneyInListVO> moneyinvo=(ArrayList<MoneyInListVO>)listVO;
+				for(MoneyInListVO vo:moneyinvo){
+				MoneyInListPO po=moneyin.findOnID(vo.getId());
+				po.setCheckType(CheckType.PASS);
+				moneyin.update(po);
+				}
+				return true;
+			case MONET_OUT_NOTE:
+				ArrayList<MoneyOutListVO> moneyoutvo=(ArrayList<MoneyOutListVO>)listVO;
+				for(MoneyOutListVO vo:moneyoutvo){
+					MoneyOutListPO po=moneyout.findOnID(vo.getId());
+					po.setCheckType(CheckType.PASS);
+					moneyout.update(po);
+				}
+				return true;
+			case IN_STOREAGE_NOTE:
+				ArrayList<RepertoryInVO> repertoryinvo=(ArrayList<RepertoryInVO>)listVO;
+				for(RepertoryInVO vo:repertoryinvo){
+					RepertoryInPO po=repertoryin.findOnID(vo.getId());
+					po.setCheckType(CheckType.PASS);
+					repertoryin.update(po);
+				}
+				return true;
+			case OUT_STOREAGE_NOTE:
+				ArrayList<RepertoryOutVO> repertoryoutvo=(ArrayList<RepertoryOutVO>)listVO;
+				for(RepertoryOutVO vo:repertoryoutvo){
+					RepertoryOutPO po=repertoryout.findOnID(vo.getId());
+					po.setCheckType(CheckType.PASS);
+					repertoryout.update(po);
+				}
+				return true;
+			case SENDING_NOTE:
+				ArrayList<OrderListVO> ordervo=(ArrayList<OrderListVO>)listVO;
+				for(OrderListVO vo:ordervo){
+					OrderListPO po=order.find(vo.getBarCode());
+					po.setCheckType(CheckType.PASS);
+					order.update(po);
+				}
+				return true;
+			case LOADING_NOTE:
+				ArrayList<LoadListVO> loadvo=(ArrayList<LoadListVO>)listVO;
+				for(LoadListVO vo:loadvo){
+					LoadListPO po=load.find(vo.getId());
+					po.setCheckType(CheckType.PASS);
+					load.update(po);
+				}
+				return true;
+			case BUSINESSHALL_ARRIVAL_NOTE:
+				ArrayList<HallArrivalListVO> hallarrivalvo=(ArrayList<HallArrivalListVO>)listVO;
+				for(HallArrivalListVO vo:hallarrivalvo){
+					HallArrivalListPO po=hallarrival.find(vo.getId());
+				    po.setCheckType(CheckType.PASS);
+				    hallarrival.update(po);	
+				}
+				return true;
+			case DELIVERYING_NOTE:
+				ArrayList<DeliveringListVO> delivervo=(ArrayList<DeliveringListVO>)listVO;
+				for(DeliveringListVO vo:delivervo){
+					DeliveringListPO po=deliver.find(vo.getId());
+					po.setCheckType(CheckType.PASS);
+					deliver.update(po);
+				}
+				return true;
+			case CENTER_ARRIVAL_NOTE:
+				ArrayList<TransArrivalListVO> transarrivalvo=(ArrayList<TransArrivalListVO>)listVO;
+				for(TransArrivalListVO vo:transarrivalvo){
+					TransArrivalListPO po=transarrival.find(vo.getId());
+					po.setCheckType(CheckType.PASS);
+					transarrival.update(po);
+				}
+				return true;
+			case TRANSIT_NOTE:
+				ArrayList<TransShipmentListVO> transshipmentvo=(ArrayList<TransShipmentListVO> )listVO;
+				for(TransShipmentListVO vo:transshipmentvo){
+					TransShipmentListPO po=transshipment.find(vo.getTransitDocNumber());
+					po.setCheckType(CheckType.PASS);
+					transshipment.update(po);
+				}
+				return true;
+			}
+		}catch(RemoteException e){
+			e.printStackTrace();
+		}
 		return false;
 	}
 
 	public boolean failSelectedDoc(DocType docType, ArrayList<? extends ListVO> listVO) {
 		// TODO Auto-generated method stub
+		try{
+			switch(docType){
+			case MONEY_IN_NOTE:
+				ArrayList<MoneyInListVO> moneyinvo=(ArrayList<MoneyInListVO>)listVO;
+				for(MoneyInListVO vo:moneyinvo){
+				MoneyInListPO po=moneyin.findOnID(vo.getId());
+				po.setCheckType(CheckType.NOTPASS);
+				moneyin.update(po);
+				}
+				return true;
+			case MONET_OUT_NOTE:
+				ArrayList<MoneyOutListVO> moneyoutvo=(ArrayList<MoneyOutListVO>)listVO;
+				for(MoneyOutListVO vo:moneyoutvo){
+					MoneyOutListPO po=moneyout.findOnID(vo.getId());
+					po.setCheckType(CheckType.NOTPASS);
+					moneyout.update(po);
+				}
+				return true;
+			case IN_STOREAGE_NOTE:
+				ArrayList<RepertoryInVO> repertoryinvo=(ArrayList<RepertoryInVO>)listVO;
+				for(RepertoryInVO vo:repertoryinvo){
+					RepertoryInPO po=repertoryin.findOnID(vo.getId());
+					po.setCheckType(CheckType.NOTPASS);
+					repertoryin.update(po);
+				}
+				return true;
+			case OUT_STOREAGE_NOTE:
+				ArrayList<RepertoryOutVO> repertoryoutvo=(ArrayList<RepertoryOutVO>)listVO;
+				for(RepertoryOutVO vo:repertoryoutvo){
+					RepertoryOutPO po=repertoryout.findOnID(vo.getId());
+					po.setCheckType(CheckType.NOTPASS);
+					repertoryout.update(po);
+				}
+				return true;
+			case SENDING_NOTE:
+				ArrayList<OrderListVO> ordervo=(ArrayList<OrderListVO>)listVO;
+				for(OrderListVO vo:ordervo){
+					OrderListPO po=order.find(vo.getBarCode());
+					po.setCheckType(CheckType.NOTPASS);
+					order.update(po);
+				}
+				return true;
+			case LOADING_NOTE:
+				ArrayList<LoadListVO> loadvo=(ArrayList<LoadListVO>)listVO;
+				for(LoadListVO vo:loadvo){
+					LoadListPO po=load.find(vo.getId());
+					po.setCheckType(CheckType.NOTPASS);
+					load.update(po);
+				}
+				return true;
+			case BUSINESSHALL_ARRIVAL_NOTE:
+				ArrayList<HallArrivalListVO> hallarrivalvo=(ArrayList<HallArrivalListVO>)listVO;
+				for(HallArrivalListVO vo:hallarrivalvo){
+					HallArrivalListPO po=hallarrival.find(vo.getId());
+				    po.setCheckType(CheckType.NOTPASS);
+				    hallarrival.update(po);	
+				}
+				return true;
+			case DELIVERYING_NOTE:
+				ArrayList<DeliveringListVO> delivervo=(ArrayList<DeliveringListVO>)listVO;
+				for(DeliveringListVO vo:delivervo){
+					DeliveringListPO po=deliver.find(vo.getId());
+					po.setCheckType(CheckType.NOTPASS);
+					deliver.update(po);
+				}
+				return true;
+			case CENTER_ARRIVAL_NOTE:
+				ArrayList<TransArrivalListVO> transarrivalvo=(ArrayList<TransArrivalListVO>)listVO;
+				for(TransArrivalListVO vo:transarrivalvo){
+					TransArrivalListPO po=transarrival.find(vo.getId());
+					po.setCheckType(CheckType.NOTPASS);
+					transarrival.update(po);
+				}
+				return true;
+			case TRANSIT_NOTE:
+				ArrayList<TransShipmentListVO> transshipmentvo=(ArrayList<TransShipmentListVO> )listVO;
+				for(TransShipmentListVO vo:transshipmentvo){
+					TransShipmentListPO po=transshipment.find(vo.getTransitDocNumber());
+					po.setCheckType(CheckType.NOTPASS);
+					transshipment.update(po);
+				}
+				return true;
+			}
+		}catch(RemoteException e){
+			e.printStackTrace();
+		}
 		return false;
 	}
 
 	public boolean ChangeDoc(DocType docType, ListVO listVO) {
 		// TODO Auto-generated method stub
+		try{
+			switch(docType){
+			case MONEY_IN_NOTE:
+				MoneyInListVO moneyinvo=(MoneyInListVO)listVO;
+				MoneyInListPO moneyinpo=new MoneyInListPO(moneyinvo.getId(),moneyinvo.getDate(),moneyinvo.getMoneySum(),moneyinvo.getStaffId(),moneyinvo.getBarcode(),moneyinvo.getCheckType());
+				moneyin.update(moneyinpo);
+				return true;
+			case MONET_OUT_NOTE:
+				MoneyOutListVO moneyoutvo=(MoneyOutListVO)listVO;
+				MoneyOutListPO moneyoutpo=new MoneyOutListPO(moneyoutvo.getId(), moneyoutvo.getDate(), moneyoutvo.getMoney(), moneyoutvo.getPayer(), moneyoutvo.getAccountNum(), moneyoutvo.getClause(), moneyoutvo.getNote(), moneyoutvo.getCheckType());
+				moneyout.update(moneyoutpo);
+				return true;
+			case IN_STOREAGE_NOTE:
+				RepertoryInVO repertoryinvo=(RepertoryInVO)listVO;
+				RepertoryInPO repertoryinpo=new RepertoryInPO(repertoryinvo.getId(),repertoryinvo.getNum(),repertoryinvo.getTime(),repertoryinvo.getDestnation(),repertoryinvo.getAreacode(),repertoryinvo.getRownumber(),repertoryinvo.getFramenumber(),repertoryinvo.getFramenumber(),repertoryinvo.getCheckType());
+				repertoryin.update(repertoryinpo);
+				return true;
+			case OUT_STOREAGE_NOTE:
+				RepertoryOutVO repertoryoutvo=(RepertoryOutVO)listVO;
+				RepertoryOutPO repertoryoutpo=new RepertoryOutPO(repertoryoutvo.getId(),repertoryoutvo.getCode(),repertoryoutvo.getTime(),repertoryoutvo.getDestnation(),repertoryoutvo.getTransportation(),repertoryoutvo.getTransCode(),repertoryoutvo.getVehicleCode(),repertoryoutvo.getCheckType());
+				repertoryout.update(repertoryoutpo);
+				return true;
+			case SENDING_NOTE:
+				OrderListVO ordervo=(OrderListVO)listVO;
+				OrderListPO orderpo=new OrderListPO(ordervo.getSenderName(),ordervo.getSenderAddress(),ordervo.getSenderTeleNumber(),ordervo.getReceiverName(),ordervo.getReceiverAddress(),ordervo.getReceiverTeleNumber(),ordervo.getNumber(),ordervo.getWeight(),ordervo.getVolume(),ordervo.getName(),ordervo.getCategory(),ordervo.getPkgState(),ordervo.getPackPrice(),ordervo.getBarCode(),ordervo.getPkgType(),ordervo.getDepartTime(),ordervo.getArriveTime(),ordervo.getCheckType());
+				order.update(orderpo);
+				return true;
+			case LOADING_NOTE:
+				LoadListVO loadvo=(LoadListVO)listVO;
+				LoadListPO loadpo=new LoadListPO(loadvo.getId(),loadvo.getDate(),loadvo.getHallNumber(),loadvo.getTranspotationNumber(),loadvo.getDestination(),loadvo.getCarNumber(),loadvo.getGuardMan(),loadvo.getSupercargoMan(),loadvo.getBarcodes(),loadvo.getCheckType());
+				load.update(loadpo);
+				return true;
+			case BUSINESSHALL_ARRIVAL_NOTE:
+				HallArrivalListVO hallarrivalvo=(HallArrivalListVO)listVO;
+				HallArrivalListPO hallarrivalpo=new HallArrivalListPO(hallarrivalvo.getId(),hallarrivalvo.getDate(),hallarrivalvo.getTransferNumber(),hallarrivalvo.getFrom(),hallarrivalvo.getState(),hallarrivalvo.getBarCodes(),hallarrivalvo.getCheckType());
+				hallarrival.update(hallarrivalpo);
+				return true;
+			case DELIVERYING_NOTE:
+				DeliveringListVO delivervo=(DeliveringListVO)listVO;
+				DeliveringListPO deliverpo=new DeliveringListPO(delivervo.getId(),delivervo.getDate(),delivervo.getBarCode(),delivervo.getDeliveryMan(),delivervo.getCheckType());
+				deliver.update(deliverpo);
+				return true;
+			case CENTER_ARRIVAL_NOTE:
+				TransArrivalListVO transarrivalvo=(TransArrivalListVO)listVO;
+				ArrayList<GoodsInfoPO> goodpo=new ArrayList<GoodsInfoPO>();
+				for(GoodsInfoVO goodvo:transarrivalvo.getGoodsInfoVOs()){
+					goodpo.add(new GoodsInfoPO(goodvo.getBarcode(), goodvo.getState(), goodvo.getDeparturePlace()));
+				}
+				TransArrivalListPO transarrivalpo=new TransArrivalListPO(transarrivalvo.getId(),transarrivalvo.getTransferNumber(),transarrivalvo.getCenterNumber(),transarrivalvo.getDate(),goodpo,transarrivalvo.getCheckType());
+				transarrival.update(transarrivalpo);
+				return true;
+			case TRANSIT_NOTE:
+				TransShipmentListVO transshipmentvo=(TransShipmentListVO)listVO;
+				TransShipmentListPO transshipmentpo=new TransShipmentListPO(transshipmentvo.getDate(),transshipmentvo.getTransitDocNumber(),transshipmentvo.getFlightNumber(),transshipmentvo.getDeparturePlace(),transshipmentvo.getDesitination(),transshipmentvo.getContainerNumber(),transshipmentvo.getSupercargoMan(),transshipmentvo.getBarcodes(),transshipmentvo.getCheckType());
+				transshipment.update(transshipmentpo);
+				return true;
+			}
+		}catch(RemoteException e){
+			e.printStackTrace();
+		}
 		return false;
 	}
 
