498ede21598c0eb9d1b0eef0d69567d5e306a37b
abc <947106974@qq.com>
2015-12-02 22:44:42 +0800

    查询中转单和装车单以返回单号的方法写好

--- Logistics_System/Client/src/main/java/businesslogic/logisticsbl/TransferCenterReceiveBLImpl.java
+++ Logistics_System/Client/src/main/java/businesslogic/logisticsbl/TransferCenterReceiveBLImpl.java
@@ -5,13 +5,18 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
+import po.list.LoadListPO;
 import po.list.OrderListPO;
 import po.list.TransArrivalListPO;
+import po.list.TransShipmentListPO;
 import po.repertory.GoodsInfoPO;
 import po.system.SystemLogPO;
 import presentation.mainui.CurrentUser;
+import dataservice.agency.AgencyDataService;
+import dataservice.list.LoadListDataService;
 import dataservice.list.OrderListDataService;
 import dataservice.list.TransArrivalListDataService;
+import dataservice.list.TransShipmentListDataService;
 import dataservice.system.SystemLogDataService;
 import utility.ResultMessage;
 import vo.GoodsInfoVO;
@@ -24,14 +29,21 @@ public class TransferCenterReceiveBLImpl implements TransferCenterReceiveBLServi
      OrderListDataService orderlist=null;
      TransArrivalListDataService transarrival=null;
      CurrentUser user=null;
+     LoadListDataService loadlist=null;
  	SystemLogDataService system=null;
+ 	AgencyDataService agency=null;
+ 	TransShipmentListDataService trans=null;
  	SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+ 	
  	
      public TransferCenterReceiveBLImpl(CurrentUser currentuser){
         orderlist=(OrderListDataService)RMIHelper.find("OrderListDataService");
         transarrival=(TransArrivalListDataService)RMIHelper.find("TransArrivalListDataService");
         user=currentuser;
-        system=(SystemLogDataService)RMIHelper.find("SystemLogDataService");    	   
+        system=(SystemLogDataService)RMIHelper.find("SystemLogDataService"); 
+        loadlist=(LoadListDataService)RMIHelper.find("LoadListDataService");
+        agency=(AgencyDataService)RMIHelper.find("AgencyDataService");
+        trans=(TransShipmentListDataService)RMIHelper.find("TransShipmentListDataService");
         }
 	public ResultMessage createTransArrivalList(TransArrivalListVO transArrivalList) {
 		// TODO Auto-generated method stub
@@ -63,10 +75,38 @@ public class TransferCenterReceiveBLImpl implements TransferCenterReceiveBLServi
 	}
 	public TransArrivalVO getLoadList(String loadlistid) {
 		// TODO Auto-generated method stub
+		LoadListPO loadlistpo=null;
+		try{
+			loadlistpo=loadlist.find(loadlistid);
+			if(loadlistpo==null)
+				return null;
+			else{
+				TransArrivalVO transvo=new TransArrivalVO();
+				transvo.barcodes=loadlistpo.getBarcodes();
+				transvo.depatureplace=agency.find(loadlistpo.getHallNumber()).getAgencyName();
+				return transvo;
+			}
+			}catch(RemoteException e){
+				e.printStackTrace();
+			}
 		return null;
 	}
 	public TransArrivalVO getTransShipmentList(String transshipmentlistid) {
 		// TODO Auto-generated method stub
+		TransShipmentListPO transshipmentpo=null;
+		try{
+			transshipmentpo=trans.find(transshipmentlistid);
+			if(transshipmentpo==null)
+				return null;
+			else{
+				TransArrivalVO transvo=new TransArrivalVO();
+				transvo.barcodes=transshipmentpo.getBarcodes();
+				transvo.depatureplace=agency.find(transshipmentpo.getTransitDocNumber().substring(0,6)).getAgencyName();
+				return transvo;
+			}
+		}catch(RemoteException e){
+			e.printStackTrace();
+		}
 		return null;
 	}
 
