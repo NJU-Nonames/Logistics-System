600df4e571f27e45c061e369b913eeccd217bad9
WJerry <wangjiawei0227@163.com>
2015-12-08 18:53:56 +0800

    背景测试

--- Logistics_System/Client/src/main/java/presentation/courierui/ReceivePkg.java
+++ Logistics_System/Client/src/main/java/presentation/courierui/ReceivePkg.java
@@ -75,15 +75,18 @@ public class ReceivePkg extends JPanel{
 	protected void paintComponent(Graphics g){
         super.paintComponent(g);
         setBackground(Color.WHITE);
-        g.drawImage(Img.BACKGROUND, CourierFrame.w/6, CourierFrame.h/6, CourierFrame.w, CourierFrame.h, null);
+        g.drawImage(Img.BACKGROUND2, CourierFrame.w/6, CourierFrame.h/6, CourierFrame.w, CourierFrame.h, null);
+        g.drawImage(Img.BACKGROUND1, CourierFrame.w/6, 0, CourierFrame.w, CourierFrame.h/6, null);
+        g.drawImage(Img.BACKGROUND0, 0, 0, CourierFrame.w/6, CourierFrame.h, null);
         g.drawLine(CourierFrame.w/6, 10, CourierFrame.w/6, CourierFrame.h-10);
         g.drawLine(CourierFrame.w/6+10, CourierFrame.h/6, CourierFrame.w, CourierFrame.h/6);
         g.drawLine(CourierFrame.w/6+20, 128+80+30, CourierFrame.w-20, 128+80+30);
         g.drawLine(CourierFrame.w/6+20, 128+80+150+30, CourierFrame.w-20, 128+80+150+30);
 
         if(willprintMessage){
-        	g.drawImage(Img.BACKGROUND, CourierFrame.w/6, CourierFrame.h/6, CourierFrame.w, CourierFrame.h, null);
-        	
+        	g.drawImage(Img.BACKGROUND2, CourierFrame.w/6, CourierFrame.h/6, CourierFrame.w, CourierFrame.h, null);
+            g.drawImage(Img.BACKGROUND1, CourierFrame.w/6, 0, CourierFrame.w, CourierFrame.h/6, null);
+            g.drawImage(Img.BACKGROUND0, 0, 0, CourierFrame.w/6, CourierFrame.h, null);
             g.setColor(co);
             g.setFont(new Font("宋体", Font.BOLD, 26));
             g.drawString(result, -result.length()*13+CourierFrame.w/2, 13+CourierFrame.h-30);
