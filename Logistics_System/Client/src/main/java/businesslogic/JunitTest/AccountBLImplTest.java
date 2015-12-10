e6f71596adc702bf2b857135dfb4b882d2470b2f
abc <947106974@qq.com>
2015-12-10 19:49:00 +0800

    1

--- Logistics_System/Client/src/main/java/businesslogic/JunitTest/AccountBLImplTest.java
+++ Logistics_System/Client/src/main/java/businesslogic/JunitTest/AccountBLImplTest.java
@@ -4,21 +4,27 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import businesslogic.financebl.AccountBLImpl;
+import po.agency.BankAccountPO;
+import presentation.mainui.CurrentUser;
+import utility.ResultMessage;
+import vo.BankAccountVO;
+
 public class AccountBLImplTest {
+	AccountBLImpl accountbl=new AccountBLImpl(new CurrentUser("上官","快递中心","000000","shangguan"));
 
-	@Test
-	public void testAccountBLImpl() {
-		fail("尚未实现");
-	}
-
+	
 	@Test
 	public void testCreateCount() {
-		fail("尚未实现");
+		BankAccountVO bankaccountvo=new BankAccountVO("银行账户测试1", "0000000000000000099", 100);
+		assertEquals(new ResultMessage(true, "添加账户成功!"), accountbl.createCount(bankaccountvo));
+		assertEquals(new ResultMessage(false, "账户已存在!"), accountbl.createCount(bankaccountvo));
 	}
 
 	@Test
 	public void testRemoveCount() {
-		fail("尚未实现");
+		assertEquals(new ResultMessage(false, "删除账户成功!"), accountbl.removeCount("0000000000000000099"));
+		assertEquals(new ResultMessage(false, "要删除的账户不存在!"), accountbl.removeCount("0000000000000000099"));
 	}
 
 	@Test
